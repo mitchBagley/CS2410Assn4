6af31f3cb5c8a62df0e4d1a6a6defd409d97c9c8
Jonathan Bagley <a02337183@aggies.usu.edu>
2023-03-12 19:15:47 -0600

    Edited all of them to make list items removable

--- Recipe.js
+++ Recipe.js
@@ -1,108 +1,56 @@
-//adding all input fields for the ingredients methods
-const ingrInput = document.getElementById("ingredient");
-const addIngr = document.getElementById("add-ingr-button");
-const remIngr = document.getElementById("rem-ingr-button");
-const resetIngrs = document.getElementById("reset-ingrs");
-const ingrItem_listOfIngrs = document.getElementById("list-of-ingrs");
+//adding all input fields for all methods methods
+const itemInput = document.getElementById("item");
+const addItem = document.getElementById("add-item-button");
+const resItems = document.getElementById("reset-items");
+const item_listOfItems = document.getElementById("list-of-items");
 
 //declaring the ingredient array
-const ingrArray = [];
+const itemArray = [];
+const resetItems = document.getElementsByClassName("items");
 
-//Adding user put ingredients to the ingredients list box
-addIngr.addEventListener("click", () => 
+//Adding user put ingredients to the list box
+addItem.addEventListener("click", () => 
 {
-    let input = ingrInput.value;
-    ingrArray.push(input);
-    console.log(ingrArray);
-    const ingrItem = document.createElement("li");
-    ingrItem_listOfIngrs.appendChild(ingrItem);
-    ingrItem.innerText = input;
+    let input = itemInput.value;
+    itemArray.push(input);
+    console.log(itemArray);
+    const item = document.createElement("li");
+    item_listOfItems.appendChild(item);
+    item.innerText = input;
 });
 
-//allowing the enter key to work on the ingredient text bar
+//allowing the enter key to work on the text bar
 function handleKeyDown(event) 
 {
     if (event.keyCode === 13) 
     {
-        let input = ingrInput.value;
-        ingrArray.push(input);
-        console.log(ingrArray);
-        const ingrItem = document.createElement("li");
-        ingrItem_listOfIngrs.appendChild(ingrItem);
-        ingrItem.innerText = input;
+        let input = itemInput.value;
+        itemArray.push(input);
+        console.log(itemArray);
+        const item = document.createElement("li");
+        item_listOfItems.appendChild(item);
+        item.innerText = input;
     }
 }
+
+//Making list items removable
+
+item_listOfItems.addEventListener("click", (theClick) =>
+{
+    const itemClicked = theClick.target;
+    itemClicked.remove();
+});
+
   
-//Removing user put ingredients to the ingredients list box
-remIngr.addEventListener("click", () => 
+
+//Resetting user put input to list box
+
+resItems.addEventListener("click", () => 
 {
-    
-});
-
-//Removing user put ingredients to the ingredients list box
-resetIngrs.addEventListener("click", () => 
-{
-    let resIngrs = document.getElementsByClassName("ingrs");
-    for (i = 0; i < resIngrs.length; i++)
-    {
-        resIngrs[i].remove();
-    }
-});
-
-
-
-
-
-//adding all input fields for the instructions methods
-const instInput = document.getElementById("instructions");
-const addInst = document.getElementById("add-inst-button");
-const remInst = document.getElementById("rem-inst-button");
-const resetInsts = document.getElementById("reset-insts");
-const instItem_listOfInsts = document.getElementById("list-of-insts");
-
-//declaring the instructions array
-const instArray = [];
-
-//Adding user put ingredients to the ingredients list box
-addInst.addEventListener("click", () => 
-{
-    let userInput = instInput.value;
-    instArray.push(userInput);
-    const instItem = document.createElement("li");
-    instItem_listOfInsts.appendChild(instItem);
-    instItem.innerText = userInput;
-
-
-});
-
- //allowing the enter key to work on the instructions text bar
-    // function handleKeyDown(event) 
-    // {
-    //     if (event.keyCode === 13) 
-    //     {
-    //     let userInput = instInput.value;
-    //     instArray.push(userInput);
-    //     const instItem = document.createElement("li");
-    //     instItem_listOfInsts.appendChild(instItem);
-    //     instItem.innerText = userInput;
-    //     }
-    // }
-
-
-//Removing user put ingredients to the ingredients list box
-remInst.addEventListener("click", () => 
-{
-    
-});
-
-//Removing user put ingredients to the ingredients list box
-resetInsts.addEventListener("click", () => 
-{
-    let resInsts = document.getElementsByClassName("insts");
-    for (i = 0; i < resInsts.length; i++)
-    {
-        resInsts[i].remove();
-    }
+     for (i = 0; i < resetItems.length; i++)
+     {
+         resetItems[i].remove();
+     }
 });
 
 //making the recipe download button 
@@ -113,3 +61,6 @@ makeRecipe.addEventListener("click", () =>
 {
     writeRecipeToFile(); //doesn't work as of now
 });
+
+
+
